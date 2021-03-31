d0fc29271ecbe5b32817eb97ebbd1a14c58b4535
Zishuo Xu <xuzishuo1996@gmail.com>
2021-03-31 16:27:35 +0800

    complete Block Nested Loop Join operator. timeout error

--- src/main/java/edu/berkeley/cs186/database/query/join/BNLJOperator.java
+++ src/main/java/edu/berkeley/cs186/database/query/join/BNLJOperator.java
@@ -11,6 +11,7 @@ import edu.berkeley.cs186.database.table.Record;
 /**
  * Performs an equijoin between two relations on leftColumnName and
  * rightColumnName respectively using the Block Nested Loop Join algorithm.
+ *
  */
 public class BNLJOperator extends JoinOperator {
     protected int numBuffers;
@@ -67,6 +68,7 @@ public class BNLJOperator extends JoinOperator {
             this.fetchNextLeftBlock();
 
             this.rightSourceIterator = getRightSource().backtrackingIterator();
+            // mark it to the first entry of the first right page for future reset
             this.rightSourceIterator.markNext();
             this.fetchNextRightPage();
 
@@ -85,7 +87,14 @@ public class BNLJOperator extends JoinOperator {
          * You may find QueryOperator#getBlockIterator useful here.
          */
         private void fetchNextLeftBlock() {
-            // TODO(proj3_part1): implement
+            // (proj3_part1): implement
+
+            leftBlockIterator = QueryOperator.getBlockIterator(leftSourceIterator, getLeftSource().getSchema(), numBuffers - 2);
+            // mark it to the first entry of the same block for future reset
+            leftBlockIterator.markNext();
+            if (leftBlockIterator.hasNext()) {
+                leftRecord = leftBlockIterator.next();
+            }
         }
 
         /**
@@ -99,7 +108,12 @@ public class BNLJOperator extends JoinOperator {
          * You may find QueryOperator#getBlockIterator useful here.
          */
         private void fetchNextRightPage() {
-            // TODO(proj3_part1): implement
+            // (proj3_part1): implement
+
+            rightPageIterator = QueryOperator.getBlockIterator(leftSourceIterator, getLeftSource().getSchema(), 1);
+
+            // mark it to the first entry of the page for future reset
+            rightPageIterator.markNext();
         }
 
         /**
@@ -109,10 +123,60 @@ public class BNLJOperator extends JoinOperator {
          * You may find JoinOperator#compare useful here. (You can call compare
          * function directly from this file, since BNLJOperator is a subclass
          * of JoinOperator).
+         *
+         *  Pseudocode:
+         *  for each block of B−2 pages B r in R:       --- loop 1
+         *      for each page p s in S:                 --- loop 2
+         *          for each record r i in B r :        --- loop 3
+         *              for each record s j in p s :    --- loop 4
+         *                  if θ(r i ,s j ):
+         *                      yield <r i , s j >
          */
         private Record fetchNextRecord() {
-            // TODO(proj3_part1): implement
-            return null;
+            // (proj3_part1): implement
+
+            // 4 cases (from inner loop to outer loop), see the pseudocode in above
+            while (true) {
+                // Case 1 (for loop 4): The right page iterator has a value to yield
+                if (rightPageIterator.hasNext()) {
+                    nextRecord = rightPageIterator.next();
+                    if (compare(leftRecord, nextRecord) == 0) {
+                        return leftRecord.concat(nextRecord);
+                    }
+                }
+                // Case 2 (for loop 3): The right page iterator doesn't have a value to yield but the left block iterator does
+                else if (leftBlockIterator.hasNext()) {
+                    // [advance left]: within block
+                    leftRecord = leftBlockIterator.next();
+
+                    // [reset right]: reset right record iter to the first record of the same page
+                    rightPageIterator.reset();
+                }
+                // Case 3 (for loop 2): Neither the right page nor left block iterators have values to yield,
+                // but there's more right pages
+                else if (rightSourceIterator.hasNext()) {
+                    // [reset left]: reset left record iter to the first record in the curr block of left pages
+                    leftBlockIterator.reset();
+
+                    // [advance right]
+                    fetchNextRightPage();
+                }
+                // Case 4 (for loop 1): Neither right page nor left block iterators have values nor are there more right pages,
+                // but there are still left blocks
+                else if (leftSourceIterator.hasNext()) {
+                    // [advance left]: to the next block
+                    fetchNextLeftBlock();
+                    leftRecord = leftBlockIterator.next();
+
+                    // [reset right]: reset right record iter to the first record of the FIRST page
+                    rightSourceIterator.reset();    // has marked it in the constructor
+                    fetchNextRightPage();
+                }
+                // Upon completion
+                else {
+                    return null;
+                }
+            }
         }
 
         /**
